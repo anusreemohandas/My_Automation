cbe58d653eb2f8ea0d4be927be7f2b8fa2b1b25f
Anusree <anusreedas.sree7@gmail.com>
2024-02-20 14:56:37 +0530

    all files commit

--- 7rmart/src/main/java/utilities/GeneralUtility.java
+++
@@ -1,8 +0,0 @@
-package utilities;
-
-public class GeneralUtility {
-	
-	public static final String TESTDATAFILE = System.getProperty("user.dir")+ "\\src\\main\\resources\\TestData.xlsx";
-	public static final String PROMOCODE_IMG = System.getProperty("user.dir")+ "\\src\\main\\resources\\promoCode_ManageOfferCode.png";
-	public static final String SLIDER_IMG = System.getProperty("user.dir")+ "\\src\\main\\resources\\iphone_MobileSlider.png";
-}
